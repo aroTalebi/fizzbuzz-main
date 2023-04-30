d9b1c08e3d7927ab88977b18d89e258aae24b7f2
Arash Talebi <arash8889@gmail.com>
2023-04-30 10:40:32 +0200

    Error return message created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,38 @@
-import styles from "@styles/Home.module.css";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 import Head from "next/head";
 
-export const EMPTY_RESULT_HINT =
-  "Geben Sie einen Werte > 1 ein in das Formular ein.";
-
 function Home() {
+  const EMPTY_RESULT_HINT = "Bitte geben Sie eine Zahl größer als 0 ein";
+
+  const [number, setNumber] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const num = parseInt(inputValue);
+
+    if (num < 1) {
+      setResults([]);
+      return;
+    }
+
+    const newResults = [];
+    for (let i = 1; i <= num; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        newResults.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+        newResults.push("Fizz");
+      } else if (i % 5 === 0) {
+        newResults.push("Buzz");
+      } else {
+        newResults.push(i);
+      }
+    }
+    setResults(newResults);
+  };
+
   return (
     <>
       <Head>
@@ -18,8 +44,8 @@ function Home() {
       <main className="p-5">
         <h1 className="text-primary">FizzBuzz - Bewerber Quiz</h1>
         <div className="form w-25">
-          <form>
-            <label for="number" className="form-label">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="number" className="form-label">
               Bitte geben sie eine nummer ein:
             </label>
             <input
@@ -27,11 +53,15 @@ function Home() {
               className="form-control"
               id="number"
               placeholder="Enter number"
-              name="email"
+              name="number"
+              onChange={(e) => setNumber(e.target.value)}
             />
-            <button type="submit" class="btn btn-sm btn-primary mt-2">
-            Submit
-          </button>
+            <small className="text-danger">
+              {number && number < 0 ? EMPTY_RESULT_HINT : ""}
+            </small>
+            <button type="submit" className="btn btn-sm btn-primary mt-2">
+              Submit
+            </button>
           </form>
         </div>
       </main>
